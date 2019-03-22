9db0a5f3082c54e34ab9a39fde0ef73f82cd89fe
Daniel Hernqvist <daniel.hernqvist@chasacademy.se>
2019-03-22 23:24:07 +0000

    Add layout structure to the views, Also add Adminer to the project

--- resources/views/todos/show.blade.php
+++ resources/views/todos/show.blade.php
@@ -1,17 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" crossorigin="anonymous">
-  <title>Todo Item</title>
-</head>
+@section('title')
+  Single Todo {{ $todo->name }}
+@endsection
 
-<body>
-  <div class="container">
-    <h1 class="text-center my-5">
+@section('content')
+
+<h1 class="text-center my-5">
       {{ $todo->name }}
     </h1>
     <div class="row justify-content-center">
@@ -26,6 +21,5 @@
         </div>
       </div>
     </div>
-  </div>
-</body>
-</html> 
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,4 @@ Route::get('todos', 'TodosController@index');
 
 Route::get('todos/{todo}', 'TodosController@show');
 
+Route::any('adminer', '\Miroc\LaravelAdminer\AdminerAutologinController@index');
