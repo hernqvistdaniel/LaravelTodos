be2809177be93e5e2dc9ce3dafda54e29ca44633
Daniel Hernqvist <daniel.hernqvist@chasacademy.se>
2019-03-22 10:35:05 +0000

    Add seeders to get some fake todos to work with and display them in todos-blade-view

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -4,9 +4,18 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" crossorigin="anonymous">
   <title>Todos</title>
 </head>
 <body>
   <h1>Todos Page</h1>
+  <ul>
+  @foreach($todos as $todo)
+    <li>
+    Name: {{ $todo->name }}
+    desc: {{ $todo->description }}
+    </li>
+  @endforeach
+  </ul>
 </body>
 </html>
